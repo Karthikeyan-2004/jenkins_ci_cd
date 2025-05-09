10a4776e85b82d6ed13056c48c278bc571cab250
karthikeyan-2004 <karthikeyanranganathan731@gmail.com>
2025-05-09 22:15:26 +0530

    changes in app.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="min-vh-100 bg-light d-flex align-items-center justify-content-center">
       <div className="text-center p-5 bg-white rounded shadow-lg">
         <h1 className="display-4 fw-bold text-primary mb-4">
-          Hello karthikeyan (changes here)- CI/CD with Jenkins
+          Hello karthikeyan R(changes here)- CI/CD with Jenkins
         </h1>
         <p className="text-muted">
           Welcome to a simple one-page React application styled with Bootstrap.
